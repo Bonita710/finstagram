b2ac27f815773838ec5666f0b735e289fe862f3a
Bonita710 <ztylj710@gmail.com>
2022-02-15 01:44:25 +0000

    w5d1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,8 @@ class User < ActiveRecord::Base
     has_many :comments
     has_many :likes
 end
+
+class User < ActiveRecord::Base
+    validates :email, :username, uniqueness:true
+    validates :email, :avatar_url, :username, :password, presence: true
+end
